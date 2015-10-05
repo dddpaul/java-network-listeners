c26437551ccc8191362424faf066f34d050d6cda
Pavel Derendyaev <pderendyaev@smile-net.ru>
2015-10-05 18:01:19 +0300

    Add CompletableFuture listener. Bump to 1.02.

--- src/main/java/com/github/dddpaul/Listeners.java
+++ src/main/java/com/github/dddpaul/Listeners.java
@@ -1,5 +1,7 @@
 package com.github.dddpaul;
 
+import org.jooq.lambda.Unchecked;
+
 import java.io.IOException;
 import java.net.BindException;
 import java.net.InetSocketAddress;
@@ -10,6 +12,7 @@ import java.nio.channels.SocketChannel;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 
 public class Listeners
 {
@@ -31,23 +34,17 @@ public class Listeners
      */
     public static Callable<Socket> createNonBlockingListener( final int port )
     {
-        return () -> {
-            Socket socket = null;
-            try( ServerSocketChannel serverChannel = ServerSocketChannel.open() ) {
-                serverChannel.configureBlocking( false );
-                serverChannel.socket().bind( new InetSocketAddress( port ) );
-                while( true ) {
-                    SocketChannel channel = serverChannel.accept();
-                    Thread.sleep( 100 );
-                    if( channel != null ) {
-                        socket = channel.socket();
-                        break;
-                    }
-                }
-            }
-            return socket;
-        };
+        return () -> createNonBlockingSocket( port );
     }
+
+    /**
+     * Non-blocking listener created through {@link CompletableFuture}
+     */
+    public static CompletableFuture<Socket> createCompletableListener(final int port)
+    {
+        return CompletableFuture.supplyAsync(Unchecked.supplier(() -> createNonBlockingSocket(port)));
+    }
+
 
     /**
      * NetCat listener (requires installed nc binary)
@@ -70,5 +67,22 @@ public class Listeners
         } catch( BindException ignored ) {
             return false;
         }
+    }
+
+    private static Socket createNonBlockingSocket( final int port ) throws IOException, InterruptedException {
+        Socket socket;
+        try( ServerSocketChannel serverChannel = ServerSocketChannel.open() ) {
+            serverChannel.configureBlocking( false );
+            serverChannel.socket().bind( new InetSocketAddress( port ) );
+            while( true ) {
+                SocketChannel channel = serverChannel.accept();
+                Thread.sleep( 100 );
+                if( channel != null ) {
+                    socket = channel.socket();
+                    break;
+                }
+            }
+        }
+        return socket;
     }
 }
--- src/test/java/ListenersTest.java
+++ src/test/java/ListenersTest.java
@@ -1,4 +1,5 @@
 import com.github.dddpaul.Listeners;
+import org.jooq.lambda.Unchecked;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.*;
+import java.util.function.Function;
 
 public class ListenersTest extends Assert
 {
@@ -72,6 +74,39 @@ public class ListenersTest extends Assert
             Thread.sleep( 250 );
             assertTrue( Listeners.isAvailable( PORT ) );
         }
+    }
+
+    @Test
+    public void testCreateCompletableListener() throws Exception
+    {
+        CountDownLatch latch = new CountDownLatch(1);
+        Listeners.createCompletableListener( PORT ).thenAccept( Unchecked.consumer( socket -> {
+            socket.close();
+            latch.countDown();
+        }));
+        connect();
+        latch.await();
+        assertTrue( Listeners.isAvailable( PORT ) );
+    }
+
+    /**
+     * CompletableFuture is non-interruptible, see
+     * <a href="http://www.nurkiewicz.com/2015/03/completablefuture-cant-be-interrupted.html">CompletableFuture can't be interrupted</a>
+     */
+    @Test
+    public void testCancelCompletableListener() throws Exception
+    {
+        CompletableFuture<Socket> listener = Listeners.createCompletableListener( PORT );
+        try {
+            listener.get( 1, TimeUnit.SECONDS ).close();
+        } catch( TimeoutException e ) {
+            listener.cancel( true );
+            Thread.sleep( 250 );
+            assertFalse(Listeners.isAvailable(PORT));
+            connect(); // To terminate listener
+            assertTrue( Listeners.isAvailable( PORT ) );
+        }
+        assertTrue(listener.isCompletedExceptionally());
     }
 
     @Test
